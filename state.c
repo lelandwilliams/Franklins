26fc1fe5de2c42be03b88ecf2c0e9f9ee412ea89
Lee Williams <lelandhwilliams@gmail.com>
2018-10-29 19:18:00 -0500

    Initial add of state.c

---
+++ state.c
@@ -0,0 +1,13 @@
+typedef enum {ACTIVE, PASSIVE, LEADER} nodestatus;
+
+typedef struct {
+    int server_port;
+    int L_port;
+    int R_port;
+    nodestatus status ;
+    int round_number;
+    int L_ID;
+    int R_ID;
+    int my_id;
+} state;
+
