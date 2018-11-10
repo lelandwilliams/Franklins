122e86e461e608a038aa254c9e82e6632e88ff09
Lee Williams <lelandhwilliams@gmail.com>
2018-11-10 12:59:10 -0600

    Began definition of main_loop, and removed port polling messages

--- network.c
+++ network.c
@@ -1,5 +1,11 @@
 #include "network.h"
 
+void main_loop(state_t *state) {
+    int done = 0;
+    //while(done == 0) 
+     //   if(! network_complete()) {;}
+}
+
 int start_server(int last_port, state_t *state) {
     /* 
      * */
@@ -17,10 +23,10 @@ int start_server(int last_port, state_t *state) {
         server_port++;
         addr.sin_port = (in_port_t) htons(server_port);
         int result = bind(server_socket, (struct sockaddr *) &addr, sizeof(addr));
-        if (result == -1)
-            fprintf(stderr, "Can't bind socket to port\n", strerror(errno));
-        else
+        if (result != -1)
             done = 1;
+//        else
+ //           fprintf(stderr, "Can't bind socket to port\n", strerror(errno));
     }
 
     server_octets = inet_ntoa(addr.sin_addr);
