41fe1c3fcb0a408b7b1dd8cf7e678e49673054dc
Lee Williams <lelandhwilliams@gmail.com>
2018-11-10 12:56:50 -0600

    Moving definitions from state_t to globals

--- state.h
+++ state.h
@@ -2,16 +2,20 @@
 #define STATE_H
 
 typedef enum {ACTIVE, PASSIVE, LEADER} nodestatus;
+int franklin_id;
+int id = 1;
+int first_port = -1; // remembers port of first node
+int port_L = -1; // the port # of the node to the 'left', or id -1
+int port_R = -1; // the port # of the node to the 'right', or id +1
+int num_nodes = -1;
 
 struct state_s{
-    int server_port;
-    int server_socket;
     int L_port;
     int R_port;
     nodestatus status;
     int round_number;
     int my_id;
-} new_state = {0,0,0,ACTIVE,0,0};
+} new_state = {0,0,ACTIVE,0,0};
 
 typedef struct state_s state_t;
 
