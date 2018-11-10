490a17eaee484a35198fa12c4a855810c9cd420a
Lee Williams <lelandhwilliams@gmail.com>
2018-11-10 12:58:24 -0600

    Added enum, proto for establish()

--- network.h
+++ network.h
@@ -8,12 +8,15 @@
 #include <stdio.h>
 #include "state.h"
 
+typedef enum{PORT, ELECTION, DIE} message_type;
+
 int server_port;
 int server_socket;
 char* server_octets;
 
 struct sockaddr_in addr;
 
+void establish(int);
 int start_server(int, state_t*);
 void main_loop(state_t*);
 #endif
