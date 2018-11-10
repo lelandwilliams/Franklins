2ba4acdcdaa0252888393348d0a74e54d3a065aa
Lee Williams <lelandhwilliams@gmail.com>
2018-11-10 13:22:51 -0600

    added proto for send_message()

--- network.h
+++ network.h
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include "state.h"
 
-typedef enum{PORT, ELECTION, DIE} message_type;
+typedef enum{PORT, ELECTION, DIE} message_t;
 
 int server_port;
 int server_socket;
@@ -17,6 +17,7 @@ char* server_octets;
 struct sockaddr_in addr;
 
 void establish(int);
+void send_message(message_t, int);
 int start_server(int, state_t*);
 void main_loop(state_t*);
 #endif
