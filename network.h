5df62ba5a33883a462a31cd7db8483a8e099e36a
Lee Williams <lelandhwilliams@gmail.com>
2018-11-11 10:13:14 -0600

    Added definitions of parity, f_ids, election()

--- network.h
+++ network.h
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "state.h"
 
 typedef enum{PORT, ELECTION, DIE} message_t;
@@ -13,9 +14,12 @@ typedef enum{PORT, ELECTION, DIE} message_t;
 int server_port;
 int server_socket;
 char* server_octets;
+int parity;
+int f_ids[4] = {0,0,0,0};
 
 struct sockaddr_in addr;
 
+void election();
 void establish(int);
 int process_message(char*);
 void send_message(message_t, int);
