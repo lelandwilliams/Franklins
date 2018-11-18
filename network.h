24693ff0d2469fa5a6f773018e16b6f01f66fef3
Lee Williams <lelandhwilliams@gmail.com>
2018-11-17 22:36:10 -0600

    added default values

--- network.h
+++ network.h
@@ -12,18 +12,22 @@
 
 typedef enum{PORT, ELECTION, DIE} message_t;
 
-int server_port;
-int server_socket;
-char* server_octets;
-int parity;
-int f_ids[4] = {0,0,0,0};
+int leader_count = 0;
+int server_port = 0;
+int server_socket = 0;
+char* server_octets = NULL;
+int f_ids[4] = {0,0,0,0}; // keeps track of which franklin ranks
+                          // the node has recieved for each parity
 int election_round = 0;
+int f_sent[4] = {0,0,0,0}; // keeps track of which nodes this node
+                            // has sent info two in each parity
 
 struct sockaddr_in addr;
 
 void establish(int);
 int process_message(char*);
 void send_message(message_t, int,int,int);
+void send_election(int, int);
 int start_server(int);
 void main_loop();
 #endif
